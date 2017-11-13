cece7f573655abecd199a87ee0bff2951bf97718
kchen07 <32498223+kchen07@users.noreply.github.com>
2017-11-13 03:30:35 -0800

    initial commit for part 2

---
+++ iQuiz/iQuiz/QuestionViewController.swift
@@ -0,0 +1,133 @@
+//
+//  QuestionViewController.swift
+//  iQuiz
+//
+//  Created by Kelley Lu Chen on 11/12/17.
+//  Copyright © 2017 Kelley Lu Chen. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    var questions = [(questionText: String, answer: Int, answers: [String])]()
+    var questionNum = 0
+    @IBOutlet weak var questionNumber: UILabel!
+    @IBOutlet weak var questionText: UILabel!
+    @IBOutlet weak var option1: UIButton!
+    @IBOutlet weak var option2: UIButton!
+    @IBOutlet weak var option3: UIButton!
+    @IBOutlet weak var option4: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        questionText.text = questions[questionNum].questionText
+        questionNumber.text = "Question \(questionNum + 1):"
+        option1.setTitle(questions[questionNum].answers[0], for: .normal)
+        option2.setTitle(questions[questionNum].answers[1], for: .normal)
+        option3.setTitle(questions[questionNum].answers[2], for: .normal)
+        option4.setTitle(questions[questionNum].answers[3], for: .normal)
+    }
+
+    @IBAction func backPressed(_ sender: Any) {
+        let changeView = CATransition()
+        changeView.duration = 0.3
+        changeView.type = kCATransitionPush
+        changeView.subtype = kCATransitionFromLeft
+        view.window!.layer.add(changeView, forKey: kCATransition)
+        present((storyboard?.instantiateViewController(withIdentifier: "mainPage"))!, animated: false, completion: nil)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var chose1 = false
+    var chose2 = false
+    var chose3 = false
+    var chose4 = false
+    
+    @IBOutlet weak var choise2: UIButton!
+    @IBOutlet weak var choice1: UIButton!
+    @IBOutlet weak var choise3: UIButton!
+    @IBOutlet weak var choise4: UIButton!
+    @IBAction func c1(_ sender: Any) {
+        chose1 = true
+        chose2 = false
+        chose3 = false
+        chose4 = false
+        colorOption()
+    }
+    
+    @IBAction func c2(_ sender: Any) {
+        chose1 = false
+        chose2 = true
+        chose3 = false
+        chose4 = false
+        colorOption()
+    }
+    @IBAction func c3(_ sender: Any) {
+        chose1 = false
+        chose2 = false
+        chose3 = true
+        chose4 = false
+        colorOption()
+    }
+    
+    @IBAction func c4(_ sender: Any) {
+        chose1 = false
+        chose2 = false
+        chose3 = false
+        chose4 = true
+        colorOption()
+    }
+    
+    func colorOption() {
+        if (chose1) {
+            choice1.backgroundColor = UIColor.lightGray
+            choise2.backgroundColor = UIColor.clear
+            choise3.backgroundColor = UIColor.clear
+            choise4.backgroundColor = UIColor.clear
+
+        } else if (chose2) {
+            choice1.backgroundColor = UIColor.clear
+            choise2.backgroundColor = UIColor.lightGray
+            choise3.backgroundColor = UIColor.clear
+            choise4.backgroundColor = UIColor.clear
+            
+        } else if (chose3) {
+            choice1.backgroundColor = UIColor.clear
+            choise2.backgroundColor = UIColor.clear
+            choise3.backgroundColor = UIColor.lightGray
+            choise4.backgroundColor = UIColor.clear
+
+        } else {
+            choice1.backgroundColor = UIColor.clear
+            choise2.backgroundColor = UIColor.clear
+            choise3.backgroundColor = UIColor.clear
+            choise4.backgroundColor = UIColor.lightGray
+        }
+    }
+    
+    @IBAction func nextPressed(_ sender: Any) {
+        createAnswer()
+    }
+    
+    func createAnswer() {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- iQuiz/iQuiz/ViewController.swift
+++ iQuiz/iQuiz/ViewController.swift
@@ -9,29 +9,37 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     let categories = ["Mathematics", "Marvel Super Heros", "Science"]
     let descriptions = ["Math related questions", "Are you a true marvel fan?", "Prove that you are a scientist"]
     let images = ["math", "marvel", "science"]
+    var mathQuestion : [(questionText: String, answer: Int, answers: [String])] =  [(questionText: "Something?", answer: 1, answers: ["One","Two","Three", "Four"])]
+    var scienceQuestion : [(questionText: String, answer: Int, answers: [String])] =  [(questionText: "Something?", answer: 1, answers: ["One","Two","Three", "Four"])]
+    var marvelQuestion : [(questionText: String, answer: Int, answers: [String])] =  [(questionText: "Something?", answer: 1, answers: ["One","Two","Three", "Four"])]
+    var questions = [[(questionText: String, answer: Int, answers: [String])]]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        questions.append(marvelQuestion)
+        questions.append(mathQuestion)
+        questions.append(scienceQuestion)
     }
+    fileprivate var questionView: QuestionViewController?
+
     @IBAction func settingPressed(_ sender: Any) {
         let alert = UIAlertController(title: "Setting", message: "Settings go here", preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
     
-      override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories.count
     }
     
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
         var col = UIColor(hue: 184/360, saturation: 33/100, brightness: 99/100, alpha: 1.0) /* #a9f6fc */
-
         cell.backgroundColor = col
         cell.textLabel!.text = categories[indexPath.row]
         cell.detailTextLabel!.text = descriptions[indexPath.row]
@@ -39,6 +47,27 @@ class ViewController: UITableViewController {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // do view switch when the user choose one cell
+        createQuestionView()
+        questionView?.questions = self.questions[indexPath.row]
+        let transition = CATransition()
+        transition.duration = 0.3
+        transition.type = kCATransitionPush
+        transition.subtype = kCATransitionFromRight
+        view.window!.layer.add(transition, forKey: kCATransition)
+        present(questionView!, animated: false, completion: nil)
+    }
+
+    
+    fileprivate func createQuestionView() {
+        if questionView == nil {
+            questionView =
+                storyboard?
+                    .instantiateViewController(withIdentifier: "questionView")
+                as? QuestionViewController
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
